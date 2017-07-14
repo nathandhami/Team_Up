1aade9ac2a49555c5069a414e07bbd52e81d666b
ndhami <nda24@sfu.ca>
2017-07-14 01:48:09 -0700

    Changed toggling side bar / Added close button for sidebar panel

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,16 +1,22 @@
-$(document).ready(function(){
-    $("#toggle-sidebar").on('click',function (e){
+$(document).ready(function() {
+    $('#userToggleSideBar').on('click', function(e) {
             e.preventDefault();
-            $("#wrapper-container").toggleClass("sidebarDisplayed");
-
+            $('#wrapper-container').toggleClass('sidebarDisplayed');
      });
+
+
+     $('.sidebarClose').on('click', function(e) {
+            e.preventDefault();
+            console.log('sas');
+            $('#wrapper-container').toggleClass('sidebarDisplayed');
+     });
+
 /*
      if ($("#toggle-sidebar").css("display") == "none"){
                  console.log('yes')
                 $('#wrapper.container').addClass('sidebarDisplayed');
             }
             */
-})
-
+});
 
 
