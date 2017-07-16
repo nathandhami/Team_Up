151812f6c24ee754a54e6c867480d15916c6f7f0
NoorUllah <nra22@sfu.ca>
2017-07-15 23:02:30 -0700

    refactor code

--- routes/edit.js
+++ routes/edit.js
@@ -1,22 +1,11 @@
 'use strict';
 
 const express = require('express');
-const _ = require('lodash');
 const router = new express.Router();
 
 /* GET Edit Account page */
 router.route('/editAccount')
-  .get((req, res, next) => {
-    // let displayName = null;
-    // let userImage = null;
-    //
-    // if (_.has(req, 'user')) {
-    //   displayName = req.user.displayName;
-    //   userImage = req.user.image;
-    // }
-    // const messages = req.flash('error');
-
-    console.log('HeLLO');
+  .get((req, res, next) => 
     res.render('editAccount');
     return;
   });
