ccdb4409093bd76c1b9fd059531342e51699bb33
nrandhawa <nrandhawa@phemi.com>
2017-07-17 16:21:03 -0700

    Extract contact form data in the back end for delete account

--- routes/deleteAccount.js
+++ routes/deleteAccount.js
@@ -1,11 +1,16 @@
 'use strict';
 
 const express = require('express');
+const xssFilters = require('xss-filters');
 const router = new express.Router();
 
 /* GET Edit Account page */
 router.route('/')
   .post((req, res, next) => {
+    // extract data
+    const userEmail = xssFilters.inHTMLData(req.body.userEmail);
+    const userPass = xssFilters.inHTMLData(req.body.userPass);
+
     res.redirect('/');
     return;
   });
