837643e84022be2b6ac9ca6c89be541816982549
nrandhawa <nrandhawa@phemi.com>
2017-07-17 16:12:37 -0700

    Extract contact form data in the back end

--- routes/contactUs.js
+++ routes/contactUs.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const express = require('express');
+const xssFilters = require('xss-filters');
 const router = new express.Router();
 
 /* GET Edit Account page */
 router.route('/')
   .post((req, res, next) => {
+    // extract data
+    const contactName = xssFilters.inHTMLData(req.body.contactName);
+    const contactEmail = xssFilters.inHTMLData(req.body.contactEmail);
+    const contactLocation = xssFilters.inHTMLData(req.body.contactLocation);
+    const contactMsg = xssFilters.inHTMLData(req.body.contactMsg);
+
+    // TODO: send email
+
     res.redirect('/');
     return;
   });
