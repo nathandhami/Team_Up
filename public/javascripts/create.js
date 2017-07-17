23e0c5ceed55ad1077e91aefa1c00fab08c37b04
rkongtin <rkongtin@sfu.ca>
2017-07-17 14:48:31 -0700

    added autocomplete search for locations in google maps

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,32 +1,67 @@
-var marker;
-var infowindow;
-function loadMap() {
-  var sfu = {lat: 49.277654, lng: -122.919974};
-  var mapOptions = {
-    center: sfu,
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 49.278628, lng: -122.920355},
     zoom: 12,
-    mapTypeId:google.maps.MapTypeId.ROADMAP,
-    scaleControl: true,
-  };
-  var map = new google.maps.Map(document.getElementById("map"),mapOptions);
-  marker = new google.maps.Marker({
-    position: sfu,
-    map: map,
-    animation: google.maps.Animation.DROP
+    mapTypeId: 'roadmap'
   });
-  marker.setAnimation(null);
-  marker.addListener('click', toggleBounce);
-  infowindow = new google.maps.InfoWindow({
-    content:"SFU"
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
   });
 }
-function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-    infowindow.close();
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-    infowindow.open(map, marker);
-  }
-}
-google.maps.event.addDomListener(window, 'load', loadMap);
+
