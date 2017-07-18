cd97547ccd59ab4a5b9fbae7070ac6e54a928854
NoorUllah <nra22@sfu.ca>
2017-07-18 00:03:50 -0700

    delete user from db

--- routes/deleteAccount.js
+++ routes/deleteAccount.js
@@ -2,14 +2,31 @@
 
 const express = require('express');
 const xssFilters = require('xss-filters');
+const User = require('../models/User');
 const router = new express.Router();
 
-/* GET Edit Account page */
+
 router.route('/')
   .post((req, res, next) => {
     // extract data
     const userEmail = xssFilters.inHTMLData(req.body.userEmail);
     const userPass = xssFilters.inHTMLData(req.body.userPass);
+
+    // TODO: validate password
+    User.find({email: userEmail}, (err, user) => {
+      if (err) throw err;
+
+      if (user.validPassword(userPass))  {
+        console.log('correct');
+      } 
+    });
+
+    // User.findOneAndRemove({email: userEmail}, (err) => {
+    //    if (err) throw err;
+
+    //    console.log('User ' + userEmail + 'successfully deleted!');
+    // });
+
 
     res.redirect('/');
     return;
