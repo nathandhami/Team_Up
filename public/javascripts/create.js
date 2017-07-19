fde14f2b0689e3738b3917c3349a9d4faef2a8cb
rkongtin <rkongtin@sfu.ca>
2017-07-19 00:12:34 -0700

    added textbox autofill on click events

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -2,11 +2,11 @@ var markers = [];
 
 function loadMap() {
   var locations = [
-    ['Terry Fox Field', 49.278657, -122.922332, 1],
-    ['Field 2', 49.279374, -122.927718, 1],
-    ['Forest Grove Park', 49.265672, -122.921870, 1],
-    ['SFU Fitness Centre', 49.2794424, -122.9245066, 2],
-    ['Forest Grove Park', 49.265949, -122.923910, 2]
+    ['Terry Fox Field', 'Burnaby, BC V5A 1S6', 49.278657, -122.922332, 1],
+    ['Field 2', 'Burnaby, BC V5A 1S6', 49.279374, -122.927718, 1],
+    ['Forest Grove Park', 'Burnaby, BC, Canada', 49.265672, -122.921870, 1],
+    ['SFU Fitness Centre', '8888 Gaglardi Way, Burnaby, BC V5A 1S6', 49.2794424, -122.9245066, 2],
+    ['Forest Grove Park', 'Burnaby, BC, Canada', 49.265949, -122.923910, 2]
   ];
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -30,20 +30,25 @@ function loadMap() {
   var infoWindow = new google.maps.InfoWindow();
   for (i = 0; i < locations.length; i++) {
     newMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      position: new google.maps.LatLng(locations[i][2], locations[i][3]),
       map: map,
       title: locations[i][0]
     });
-    newMarker.category = locations[i][3];
+    newMarker.category = locations[i][4];
     newMarker.setVisible(false);
-    var data = locations[i][0];
-    (function (newMarker, data) {
+    var locationName = locations[i][0];
+    var locationAddress = locations[i][1];
+    (function (newMarker, locationName) {
       google.maps.event.addListener(newMarker, 'click', function () {
-        infoWindow.setContent(data);
+        infoWindow.setContent(locationName);
         infoWindow.open(map, newMarker);
+        document.getElementById("location-name").value = locationName;
+        document.getElementById("location-name").disabled = true;
+        document.getElementById("location-address").value = locationAddress;
+        document.getElementById("location-address").disabled = true;
       });
     })
-    (newMarker, data);
+    (newMarker, locationName);
     markers.push(newMarker);
   }
 
@@ -83,4 +88,14 @@ function displayMarkers(category) {
       markers[i].setVisible(false);
     }
   }
+  if (category == 1) {
+    document.getElementById("sport").value = "Soccer";
+  } else if (category == 2) {
+    document.getElementById("sport").value = "Basketball";
+  } else if (category == 3) {
+    document.getElementById("sport").value = "Volleyball";
+  } else if (category == 4) {
+    document.getElementById("sport").value = "Baseball";
+  }
+  document.getElementById("sport").disabled = true;
 }
