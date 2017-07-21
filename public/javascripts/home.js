26d239f13ea2c2abf9a4825a3851f594be6100f6
Rafay Tanzeel <rafaytanzeel@gmail.com>
2017-07-21 08:18:05 -0700

    localised sidebar

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,22 +1,5 @@
 $(document).ready(() => {
-  $('#sidebar').on('click', (e) => {
-    e.preventDefault();
-    $('aside').toggleClass('sidebarDisplayed');
-  });
 
-
-  $('.sidebarCloseBtn').on('click', (e) => {
-    e.preventDefault();
-    $('aside').toggleClass('sidebarDisplayed');
-  });
-
-  $(window).resize(function(){
-    var minWidth = 768;
-    var container =  $('aside');
-    if(!container.hasClass('sidebarDisplayed') && $(window).width() > minWidth) {
-        container.addClass('sidebarDisplayed');
-    }
-  });
 
   // Chat Client-Side Behaviour
   let socket = io();
