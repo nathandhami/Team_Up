140ea5d95e6a78182ff9436af15fae2d0a6b4f7b
Rafay Tanzeel <rafaytanzeel@gmail.com>
2017-07-22 00:33:21 -0700

    Revert "used jquery"
    This reverts commit 2106ebd62880266dd0e18a96b91907c0c6a5787f.

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -184,14 +184,14 @@ function loadMap() {
     ['West Point Grey Park', '2250 Trimble St, Vancouver, BC V6R 4G9', 49.2664721, -123.2064366, 4]
   ];
 
-  var map = new google.maps.Map($('#map'), {
+  var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 49.278628, lng: -122.920355},
     zoom: 12,
     mapTypeId: 'roadmap'
   });
 
   // Create the search box and link it to the UI element.
-  var input = $('#pac-input');
+  var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP].push(input);
 
@@ -217,10 +217,10 @@ function loadMap() {
       google.maps.event.addListener(newMarker, 'click', function () {
         infoWindow.setContent(locationName);
         infoWindow.open(map, newMarker);
-        $("#locationName").value = locationName;
-        $("#locationName").disabled = true;
-        $("#locationAddress").value = locationAddress;
-        $("#locationAddress").disabled = true;
+        document.getElementById("locationName").value = locationName;
+        document.getElementById("locationName").disabled = true;
+        document.getElementById("locationAddress").value = locationAddress;
+        document.getElementById("locationAddress").disabled = true;
       });
     })
     (newMarker, locationName);
@@ -264,13 +264,13 @@ function displayMarkers(category) {
     }
   }
   if (category == 1) {
-    $("#sport").value = "Soccer";
+    document.getElementById("sport").value = "Soccer";
   } else if (category == 2) {
-    $("#sport").value = "Basketball";
+    document.getElementById("sport").value = "Basketball";
   } else if (category == 3) {
-    $("#sport").value = "Volleyball";
+    document.getElementById("sport").value = "Volleyball";
   } else if (category == 4) {
-    $("#sport").value = "Baseball";
+    document.getElementById("sport").value = "Baseball";
   }
-  $("#sport").disabled = true;
+  document.getElementById("sport").disabled = true;
 }
