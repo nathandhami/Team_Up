de4c86da83a92385f3f62e68e1481396226eae18
ndhami <nda24@sfu.ca>
2017-07-22 17:35:59 -0700

    Chat now properly identifies participants and can see who sends   message

--- public/javascripts/event.js
+++ public/javascripts/event.js
@@ -3,6 +3,15 @@ $(document).ready(() => {
   // Chat Client-Side Behaviour
   let socket = io();
 
+  socket.emit('new user', localUserData);
+
+  socket.on('updateChatUsers', (data) => {
+    console.log(data);
+    for (let user of data) {
+      console.log(user);
+    }
+  });
+
   /**
    * Returns a message in proper format for chat window *
    *
@@ -11,39 +20,51 @@ $(document).ready(() => {
    * @param {String} timestamp
    * @return {String}
    */
-  function generateMsg(content, img, timestamp) {
+  function generateMsg(content, name, img, timestamp) {
     let formattedMsg =
-    '<li class="clearfix">'
-    + '<span class="userImg pull-left">'
-    + '<img class="img-circle"'
-    + ' src="https://media.licdn.com/mpr/mpr/shrinknp_200_200/'
-    + 'p/2/000/079/328/1630e0b.jpg" width="28"'
-    + ' height="28"/></span><span class="userMsgBody clearfix">'
-    + '<div class="header">'
-    + '<strong>Brad</strong>'
-    + '<small class="pull-right text-muted">'
-    + '<span class="glyphicon glyphicon-time"></span>'
-    + '5 mins ago </small></div>'
-    + '<p>' + content + '</p> </span></li>';
+      '<li class="clearfix">'
+      + '<span class="userImg pull-left">'
+      + '<img class="img-circle"'
+      + ' src=' + '"' + img + '"'
+      + ' width="42"'
+      + ' height="42"/></span><span class="userMsgBody clearfix">'
+      + '<div class="header">'
+      + '<strong>' + name + '</strong>'
+      + '<small class="pull-right text-muted">'
+      + '<span class="glyphicon glyphicon-time"></span>'
+      + '5 mins ago </small></div>'
+      + '<p>' + content + '</p> </span></li>';
 
+    console.log(formattedMsg);
     return formattedMsg;
   }
 
   $('form').submit(() => {
-    socket.emit('chat message', $('#inputSendMsg').val());
+    let messageContent = $('#inputSendMsg').val();
+    // Prepare data transfer
+    let data = {
+      message: messageContent,
+      name: localUserData.name,
+      image: localUserData.img
+    };
+
+    console.log(data);
+    // Send data to server socket
+    socket.emit('chat message', data);
     $('#inputSendMsg').val('');
     // Prevent form default behaviour,
     return false;
   });
 
-  socket.on('chat message', (msg) => {
+  socket.on('chat message', (data) => {
     let msgBody = $('.chatUI-msgBody');
+
     // Fix later, security vulnerability
-    msgBody.find('ul').append(generateMsg(msg));
+    msgBody.find('ul').append(generateMsg(data.message, data.name, data.image));
     // AutoScroll
     msgBody.scrollTop(msgBody.prop('scrollHeight'));
   });
-    // END OF Chat Client-Side Behaviour
+  // END OF Chat Client-Side Behaviour
 
 });
 
--- socketApi.js
+++ socketApi.js
@@ -4,19 +4,43 @@ const io = require('socket.io')();
 
 const socketApi = {};
 const debugPrefix = 'SocketAPI: ';
+const elementNotFound = -1;
 
 socketApi.io = io;
 
-// event handler
+// Stores users that are connected @ default namespace @ default room
+let users = [];
+
+// event handler for each connected socket
 io.on('connection', (socket) => {
     console.log(debugPrefix + 'A user connected');
-    socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
-  });
+
+    socket.on('new user', function (userData, callback) {
+        if (users.indexOf(userData.name) != elementNotFound) {
+            return false;
+        }
+        else {
+            socket.userName = userData.name;
+            users.push(socket.userName);
+            io.emit('updateChatUsers', users);
+        }
+
+        console.log('New event show users: ' + users);
+    });
+
+    socket.on('chat message', (data) => {
+        console.log('Chat Message Event: ' + data.message);
+        console.log('Chat Message Event: ' + data.image);
+        
+        // Emit chat message to every client in room
+        io.emit('chat message', data);
+    });
 
     socket.on('disconnect', () => {
         console.log(debugPrefix + 'A user disconnected');
+        users.splice(users.indexOf(socket.userName), 1);
+        io.emit('updateChatUsers', users);
+        console.log('Disconnect event show users: ' + users);
     });
 });
 
