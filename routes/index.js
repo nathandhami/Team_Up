1e05faa3ff27bdb990e01c5725f8af4a11a41a7b
ndhami <nda24@sfu.ca>
2017-07-27 00:24:22 -0700

    Passed in events object to template for displaying events

--- routes/index.js
+++ routes/index.js
@@ -10,27 +10,36 @@ router.route('/')
     const messagesErrSignIn = req.flash('error');
     const messagesErrRegister = req.flash('registerError');
     const messages = messagesErrSignIn.concat(messagesErrRegister);
-    let userEvents = {};
+    
 
     // Execute DB queries if there is no error or user is defined
-    if (_.has(req,'user') && messages.length <= 0){
+    if (_.has(req, 'user') && messages.length <= 0) {
       const Event = require('../models/Event');
 
       // Get user specific events
-      Event.find({createdBy: req.user._id}, (err, events) => {
-        userEvents = events;
-        console.log(userEvents.length);
-        console.log(userEvents);
+      Event.find({ createdBy: req.user._id }, (err, events) => {
+
+        console.log(events.length);
+        console.log(events);
+
+        res.render('index', {
+          title: 'Home',
+          csrfToken: req.csrfToken(),
+          errorExist: messages.length > 0,
+          loginErrors: messages,
+          userEvents: events,
+        });
       });
     }
-
-    res.render('index', {
-      title: 'Home',
-      csrfToken: req.csrfToken(),
-      errorExist: messages.length > 0,
-      loginErrors: messages,
-      userEvents: userEvents,
-    });
+    // login page
+    else {
+      res.render('index', {
+        title: 'Login',
+        csrfToken: req.csrfToken(),
+        errorExist: messages.length > 0,
+        loginErrors: messages,
+      });
+    }
     return;
   });
 
