c3eaf38b8910f836c82e36cb53014a655c4607b2
ndhami <nda24@sfu.ca>
2017-07-27 01:29:01 -0700

    Removed some debug code

--- routes/event.js
+++ routes/event.js
@@ -7,20 +7,12 @@ const Event = require('../models/Event');
 /* GET Event page */
 router.route('/:id')
   .get((req, res, next) => {
-    console.log('Event Room: ' + req.params.id);
-    console.log('User id that requested it Room: ' + req.user._id);
-    
-
 
     Event.findById(req.params.id).exec(function (err, event) {
       // Check if user is unauthorized to view page
       // Later will change to viewable by any user that joined the event
-          console.log('Check the creator id of event: ' + event.createdBy);
-
       if (event.createdBy.toString() != req.user._id) {
         // Send unauthorized page (403 error)
-         console.log('User is unauthorized to access event ' + req.user._id);
-
         res.send('<h1> Unauthorized. Status Code: 403 </h1>');
       }
       else {
