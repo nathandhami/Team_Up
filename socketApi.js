144c83622465687fa1d0089a1c9b37078cf82796
ndhami <nda24@sfu.ca>
2017-07-28 21:57:53 -0700

    Removed some debug code

--- socketApi.js
+++ socketApi.js
@@ -10,38 +10,32 @@ const xssFilters = require('xss-filters');
 
 socketApi.io = io;
 
-// Stores users that are connected @ default namespace @ default room
+// Stores users that are connected @ default namespace
 let users = [];
 
 // event handler for each connected socket
 io.on('connection', (socket) => {
-  console.log(debugPrefix + 'A user connected');
 
   socket.on('new user', function (userData, eventData, callback) {
     {
       socket.userName = userData.name;
+
       users.push(socket.userName);
+
       socket.room = eventData.roomId;
       socket.join(socket.room);
-
-      console.log(socket.userName + ' has joined room ' + socket.room);
       io.in(socket.room).emit('updateChatUsers', users);
     }
 
     console.log('New User Event - Global socket users: ' + users);
       // Show previous messages of chat history
-      Chat.find({'roomId':socket.room}).sort({date: -1}).limit(5).exec(function(err, historyChatMsg){
-      console.log('History:' + historyChatMsg);
+      Chat.find({'roomId':socket.room}).sort({date: -1}).exec(function(err, historyChatMsg){
       io.in(socket.room).emit('sendChatHistory', historyChatMsg);
   });
 
   });
 
   socket.on('chat message', (data) => {
-
-    console.log(socket.userName + ' has sent a message ' + data.message
-      + ' to room ' + socket.room);
-
     // Overwrite the message with special characters escaped
     data.message =  xssFilters.inHTMLData(data.message);
 
@@ -52,8 +46,6 @@ io.on('connection', (socket) => {
         image: data.image,
         roomId: socket.room,
     });
-
-    console.log('Chat DB: ' + chat);
 
     chat.save((err, chat) => {
         if (err){
@@ -69,9 +61,8 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     users.splice(users.indexOf(socket.userName), 1);
     io.in(socket.room).emit('updateChatUsers', users);
-    console.log(socket.userName + ' has left room ' + socket.room);
     socket.leave(socket.room);
-    console.log(debugPrefix + 'A user disconnected');
+    
     console.log('Disconnect Event - Global socket users: ' + users);
   });
 });
