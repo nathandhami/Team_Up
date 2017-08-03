bf3cbe9939a41cc9cc3ff2db017e2e9561d7ea95
ndhami <nda24@sfu.ca>
2017-08-02 23:52:42 -0700

    Fixed 404 custom error page (was not rendering before)

--- routes/notFound.js
+++ routes/notFound.js
@@ -5,7 +5,7 @@ const router = new express.Router();
 
 /* GET 404 page */
 router.route('*')
-  .get((req, res, next, err) => {
+  .get((req, res, next) => {
     res.status(404).render('notFound', {
       title: 'Page Not Found',
     });
