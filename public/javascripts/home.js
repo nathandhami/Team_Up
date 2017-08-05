3f93a1d382567c940ba691416cd29e600328a4ea
NoorUllah <nra22@sfu.ca>
2017-08-04 21:20:44 -0700

    modify and fill in modal values

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
 	$('.editEventBtn').click( (e) => {
+		console.log("hello");
 		let event = $(e.target).children('input').val();
+		event = JSON.parse(event);
+		console.log(event.teamupName);
+		$('#teamupName').val(event.teamupName);
 	});
 });
 
