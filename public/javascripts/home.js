1529a935632471d34250bc543fa7e8b9b9e85e27
NoorUllah <nra22@sfu.ca>
2017-08-04 21:51:45 -0700

    modify and fix date format

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,10 +1,17 @@
 $(document).ready(() => {
 	$('.editEventBtn').click( (e) => {
-		console.log("hello");
 		let event = $(e.target).children('input').val();
 		event = JSON.parse(event);
-		console.log(event.teamupName);
+
+		let from_date = event.from.split('T')[0] + ' ' + event.from.split('T')[1].split('.')[0];
+		let to_date = event.to.split('T')[0] + ' ' + event.to.split('T')[1].split('.')[0];
+
 		$('#teamupName').val(event.teamupName);
+		$('#from').val(from_date);
+		$('#to').val(to_date);
+		$('#sport').val(event.sport);
+		$('#locationName').val(event.locationName);
+		$('#locationAddress').val(event.locationAddress);
 	});
 });
 
