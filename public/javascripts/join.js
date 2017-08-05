de762949f8b48c453769f5dd63af2deb613902e4
rkongtin <rkongtin@sfu.ca>
2017-08-05 16:32:37 -0700

    reorganized and removed unnecessary code in join.js

--- public/javascripts/join.js
+++ public/javascripts/join.js
@@ -22,7 +22,7 @@ $(document).ready(() => {
                   confirmButtonText: 'Okay',
                   closeOnConfirm: true,
               });
-            } 
+            }
             else {
               swal({
                   title: response.msg,
@@ -36,7 +36,7 @@ $(document).ready(() => {
                 window.location.href = response.redirect;
               });
             }
-            
+
             },
             error: function(response) {
               console.log(response);
@@ -56,11 +56,9 @@ $(document).ready(() => {
 });
 
 // Uses Google maps callback api, no need to wait for DOM to load
-let markers = [];
-let map, infoWindow, service, currentUserLocation;
+let map, infoWindow, service;
 
 function loadMap() {
-
   let locations = $('#input_events').val();
   locations = JSON.parse(locations);
   // Create the map with a default center.
@@ -71,8 +69,88 @@ function loadMap() {
     mapTypeId: 'roadmap'
   });
   infoWindow = new google.maps.InfoWindow();
-
   service = new google.maps.places.PlacesService(map);
+
+  // Create the search box and link it to the UI element.
+  let input = document.getElementById('pac-input');
+  let searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    let places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+    // For each place, get the location.
+    let bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+  let newMarker;
+  for (let i = 0; i < locations.length; i++) {
+    let locationName = locations[i].locationName;
+    let locationAddress = locations[i].locationAddress;
+    let lat = locations[i].locationCoordinates[1];
+    let long = locations[i].locationCoordinates[0];
+    let category = locations[i].sport;
+    let teamupName = locations[i].teamupName;
+    let from_date = locations[i].from.split('T')[0] + ' ' + locations[i].from.split('T')[1].split('.')[0];;
+    let to_date = locations[i].to.split('T')[0] + ' ' + locations[i].to.split('T')[1].split('.')[0];;;
+    let content = "<b>TeamUp Name</b>: " + teamupName + "<br>" +
+                  "<b>From</b>: " + from_date + "<br>" +
+                  "<b>To</b>: " + to_date + "<br>" +
+                  "<b>Sport</b>: " + category + "<br>" +
+                  "<b>Location</b>: " + locationName + "<br>" + locationAddress +
+                  "<br><a class='directions' target='_blank' href=https://www.google.com/maps/dir//" + lat + "," + long + ">Get Directions</a>";
+    newMarker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, long),
+      map: map,
+      title: locationName,
+      animation: google.maps.Animation.DROP
+    });
+    newMarker.category = category;
+    newMarker.setVisible(true);
+    (function (newMarker, locationName, locationAddress, content, infoWindow, index) {
+      google.maps.event.addListener(newMarker, 'click', function () {
+        infoWindow.setContent(content);
+        infoWindow.open(map, newMarker);
+        let locationNameElement = $("#locationName");
+        let locationAddrElement = $("#locationAddress");
+        locationNameElement.val(locationName);
+        locationAddrElement.val(locationAddress);
+        let eventLocation = [];
+        eventLocation[0] = newMarker.getPosition().lng();
+        eventLocation[1] = newMarker.getPosition().lat();
+        let jsonGeo = JSON.stringify(eventLocation);
+        $('#map-input').attr('value', jsonGeo);
+        $('.ac-event-panel').removeClass("highlight");
+        $('.ac-event-panel').hide();
+        $('#mixin' + index).show();
+        $('#mixin' + index).addClass("highlight");
+      });
+    })
+    (newMarker, locationName, locationAddress, content, infoWindow, i);
+    markers.push(newMarker);
+  }
 
   // Locate user's location if location is turned on
   if (navigator.geolocation) {
@@ -93,115 +171,6 @@ function loadMap() {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
-
-  // Create the search box and link it to the UI element.
-  let input = document.getElementById('pac-input');
-  let searchBox = new google.maps.places.SearchBox(input);
-  map.controls[google.maps.ControlPosition.TOP].push(input);
-
-  // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
-    searchBox.setBounds(map.getBounds());
-  });
-
-  let newMarker;
-  for (let i = 0; i < locations.length; i++) {
-    let locationName = locations[i].locationName;
-    let locationAddress = locations[i].locationAddress;
-    let lat = locations[i].locationCoordinates[1];
-    let long = locations[i].locationCoordinates[0];
-    let category = locations[i].sport;
-    let teamupName = locations[i].teamupName;
-    let from_date = locations[i].from.split('T')[0] + ' ' + locations[i].from.split('T')[1].split('.')[0];;
-    let to_date = locations[i].to.split('T')[0] + ' ' + locations[i].to.split('T')[1].split('.')[0];;;
-    let content = "<b>TeamUp Name</b>: " + teamupName + "<br>" + 
-                  "<b>From</b>: " + from_date + "<br>" + 
-                  "<b>To</b>: " + to_date + "<br>" + 
-                  "<b>Sport</b>: " + category + "<br>" + 
-                  "<b>Location</b>: " + locationName + "<br>" + locationAddress + 
-                  "<br><a class='directions' target='_blank' href=https://www.google.com/maps/dir//" + lat + "," + long + ">Get Directions</a>";
-    newMarker = new google.maps.Marker({
-      position: new google.maps.LatLng(lat, long),
-      map: map,
-      title: locationName,
-      animation: google.maps.Animation.DROP
-    });
-    newMarker.category = category;
-    newMarker.setVisible(true);
-    (function (newMarker, locationName, locationAddress, content, infoWindow, index) {
-      google.maps.event.addListener(newMarker, 'click', function () {
-        infoWindow.setContent(content);
-        infoWindow.open(map, newMarker);
-        let locationNameElement = $("#locationName");
-        let locationAddrElement = $("#locationAddress");
-        locationNameElement.val(locationName);
-        locationAddrElement.val(locationAddress);
-
-        let eventLocation = [];
-        eventLocation[0] = newMarker.getPosition().lng();
-        eventLocation[1] = newMarker.getPosition().lat(); 
-        let jsonGeo = JSON.stringify(eventLocation);
-        $('#map-input').attr('value', jsonGeo);
-
-        $('.ac-event-panel').removeClass("highlight");
-        $('.ac-event-panel').hide();
-        $('#mixin' + index).show();
-        $('#mixin' + index).addClass("highlight");
-      });
-    })
-    (newMarker, locationName, locationAddress, content, infoWindow, i);
-    markers.push(newMarker);
-  }
-
-  // Listen for the event fired when the user selects a prediction and retrieve
-  // more details for that place.
-  searchBox.addListener('places_changed', function() {
-    let places = searchBox.getPlaces();
-    if (places.length == 0) {
-      return;
-    }
-
-    // For each place, get the location.
-    let bounds = new google.maps.LatLngBounds();
-    places.forEach(function(place) {
-      if (!place.geometry) {
-        console.log("Returned place contains no geometry");
-        return;
-      }
-      if (place.geometry.viewport) {
-        // Only geocodes have viewport.
-        bounds.union(place.geometry.viewport);
-      } else {
-        bounds.extend(place.geometry.location);
-      }
-    });
-    map.fitBounds(bounds);
-  });
-}
-
-//  Display the markers dynamically depending on request type
-function displayMarkersGeo(category) {
-  let request = {
-    // map.getCenter()
-    // location is retrieved from geolocation
-    location: currentLocation,
-    radius: 8047,
-    types: ['park'],
-  }
-
-  service.nearbySearch(request, function(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK){
-      for (let i =0; i < results.length; i++){
-        let location = results[i].geometry.location;
-        let marker = new google.maps.Marker({
-          map:map,
-          position: results[i].geometry.location
-        });
-      }
-
-    }
-  });
-
 }
 
 // Handle the error for user's location
