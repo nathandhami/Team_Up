1942a4d0ea13b7b7a43271aa3bd24f0c8abdb934
ndhami <nda24@sfu.ca>
2017-08-04 17:37:14 -0700

    Fixed authorization check

--- routes/event.js
+++ routes/event.js
@@ -9,21 +9,27 @@ router.route('/chatroom/:id')
   .get((req, res, next) => {
 
     const id = req.params.id;
+    const userId = req.user._id;
 
-    Event.findOne({'aliasId': id}).exec(function (err, event) {
+    Event.findOne({ 'aliasId': id }).exec(function (err, event) {
       if (err || event == null) {
         res.status(404).render('notFound', {
           title: 'Page Not Found',
         });
       }
-      // Check if user is unauthorized to view page
-      // Later will change to viewable by any user that joined the event
-      else if (event.createdBy.toString() != req.user._id) {
-        // Send unauthorized page (403 error)
-        res.send('<h1> Unauthorized. Status Code: 403 </h1>');
-      }
       else {
-        res.render('event', { csrfToken: req.csrfToken(), title: event.teamupName, event: event });
+        // Check if user belongs to the event
+        let isJoined = event.users.filter(function(value){ return value == userId;});
+
+        // Check if user is unauthorized to view page
+        if (isJoined.length <= 0) {
+          // Send unauthorized page (403 error)
+          res.send('<h1> Unauthorized. Status Code: 403 </h1>');
+        }
+        else {
+          res.render('event', { csrfToken: req.csrfToken(), title: event.teamupName, event: event });
+        }
+
       }
     });
 
